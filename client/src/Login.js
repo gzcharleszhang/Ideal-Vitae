a441fc31df5e704b84103d29c2d45e91c80fefe2
Rin Z <aaronchoo9@gmail.com>
2019-02-19 20:58:06 -0800

    Completed the outline for login

--- client/src/Login.js
+++ client/src/Login.js
@@ -3,25 +3,49 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  validateForm = () => {
+    return this.state.email.length > 0 && this.state.password.length > 0;
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  handleSubmit = event => {
+      event.preventDefault();
+  }
+
   render() {
     return (
       <Form>
-        <Form.Group controlId="formBasicEmail">
+        <Form.Group controlId="email">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control autoFocus type="email" placeholder="Enter email" value={this.state.email} onChange={this.handleChange} />
           <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
+            Use the email you signed up with!
           </Form.Text>
         </Form.Group>
 
-        <Form.Group controlId="formBasicPassword">
+        <Form.Group controlId="password">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" placeholder="Password" value={this.state.password} onChange={this.handleChange} />
         </Form.Group>
         <Form.Group controlId="formBasicChecbox">
           <Form.Check type="checkbox" label="Check me out" />
         </Form.Group>
-        <Button variant="primary" type="submit">
+        // Add an option to reset password/register
+        <Button block disabled={!this.validateForm()} variant="primary" type="submit">
           Submit
         </Button>
       </Form>
