2fbf3fe32244b4f398f0396cf179f16b489f8aaa
Rin Z <aaronchoo9@gmail.com>
2019-02-26 22:15:42 -0800

    basic set up

--- config/config.js
+++ config/config.js
@@ -1,15 +1,21 @@
 const uuid = require('uuid/v4');
 
 const config = {
-  databaseConnection: "mongodb://127.0.0.1:27017/resumeDB",
-  rptNumber: 21,:
+  database: {
+    connection: "mongodb://127.0.0.1:27017/userDataz", // TODO: get mongo atlas set up
+    requirements: { useNewUrlParser: true,
+                    useFindAndModify: false,
+                    useCreateIndex: true,
+                    poolSize: 10}
+  },
+  rptNumber: 10,
   session: {
     genid: (req) => {
       return uuid();
     },
-    secret: 'Ful15tacKav3nture93jJEJNF9WNAKNFAEFA8FE9EE2nfejnf8w',
+    secret: 'Ful15tacKav3nture93jJEJNF9WNAKNFAEFA8FE9EE2nfejnf8w', // random
     resave: false,
-    saveUninitialied: false,
+    saveUninitialized: false,
     cookies: { secure: false } // since http is only currently supported
   },
 
