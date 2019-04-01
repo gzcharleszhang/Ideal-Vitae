694cf617e6cc4094eef5b0e3eff96fe13dcab2df
Aaron <aaronchoo9@gmail.com>
2019-04-01 00:30:21 -0400

    Fixed display issue where css margin would extend past html background-colour

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,6 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import {
   HashRouter,
   Route
@@ -13,6 +11,7 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 import AddEntry from './pages/AddEntry';
+import AppMenu from './Menu'
 import './App.css';
 
 
@@ -20,29 +19,28 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="pageBackground">
-        <div className="horizontalSpace">
-          <AppBar position="static">
-            <Toolbar>
-              <IconButton color="inherit" aria-label="Menu">
-                <MenuIcon />
-              </IconButton>
-              <Typography variant="h6" color="inherit" className="horizontalSpace">
-                Simple Resumes
-              </Typography>
-              <Button color="inherit">Login</Button>
-            </Toolbar>
-          </AppBar>
-        </div>
-        <HashRouter>
+      <HashRouter>
+        <div className="pageBackground">
+          <div className="horizontalSpace">
+        {/* Move the function a menu app bar*/}
+            <AppBar position="static">
+              <Toolbar>
+                <AppMenu />
+                <Typography variant="h6" color="inherit" className="horizontalSpace">
+                  Simple Resumes
+                </Typography>
+                <Button color="inherit">Login</Button>
+              </Toolbar>
+            </AppBar>
+          </div>
           <div>
             <Route path = "/login" component = {Login}/>
             <Route path = "/register" component = {Register}/>
             <Route path = "/addEntry" component = {AddEntry}/>
             <Route path = "/dashboard" component = {Dashboard}/>
           </div>
-        </HashRouter>
-      </div>
+        </div>
+      </HashRouter>
     );
   }
 }
