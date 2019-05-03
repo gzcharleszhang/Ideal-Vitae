89793e34e0b2c20d23dc659c4360a4a0229090ad
Aaron <aaronchoo9@gmail.com>
2019-05-02 22:55:09 -0400

    change in menu bar look and slight name changes

--- client/src/Menu.jsx
+++ client/src/Menu.jsx
@@ -3,12 +3,16 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import {
   NavLink,
 } from "react-router-dom";
-import AddEntry from './pages/AddEntry';
+import AddData from './pages/AddData';
 import Dashboard from './pages/Dashboard';
-import './menu.css'
+import './menu.css';
 
 
 export default class AppMenu extends Component {
@@ -36,35 +40,64 @@ export default class AppMenu extends Component {
       anchorEl,
     } = this.state;
     return (
-      <div>
-        <IconButton
-          color="inherit"
-          aria-label="Menu"
-          aria-haspopup="true"
-          onClick={this.handleClick}
-        >
-          <MenuIcon />
-        </IconButton>
-        <Menu
-          id="simple-menu"
-          anchorEl={anchorEl}
-          open={Boolean(anchorEl)}
-          onClose={this.handleClose}
-        >
-        <NavLink
-          to="/dashboard"
-          component={Dashboard}
-        >
-          <MenuItem>Dashboard</MenuItem>
-        </NavLink>
-        <NavLink
-          to="/addEntry"
-          component={AddEntry}
-        >
-          <MenuItem>Add Entry</MenuItem>
-        </NavLink>
-       </Menu>
-      </div>
+          <AppBar
+            position="sticky"
+            color="default"
+          >
+            <Toolbar>
+              <div>
+                <IconButton
+                  color="inherit"
+                  aria-label="Menu"
+                  aria-haspopup="true"
+                  onClick={this.handleClick}
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Menu
+                  id="simple-menu"
+                  anchorEl={anchorEl}
+                  open={Boolean(anchorEl)}
+                  onClose={this.handleClose}
+                >
+                <NavLink
+                  to="/dashboard"
+                  component={Dashboard}
+                >
+                  <MenuItem>Dashboard</MenuItem>
+                </NavLink>
+                <NavLink
+                  to="/addData"
+                  component={AddData}
+                >
+                  <MenuItem>Add Data</MenuItem>
+                </NavLink>
+               </Menu>
+              </div>
+              <Typography
+                variant="h6"
+                color="inherit"
+                className="horizontalSpace"
+              >
+              </Typography>
+                <h1 className="newfont">
+                  Build a Resume
+                </h1>
+              <Typography
+                variant="h6"
+                color="inherit"
+                className="horizontalSpace"
+              >
+              </Typography>
+              <Button
+                color="inherit"
+                className="newfont"
+
+              >
+                Login
+              </Button>
+            </Toolbar>
+          </AppBar>
     )
   }
 };
